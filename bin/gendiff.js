0aab5634e1fc5314247fd925e7e4e5d6833d8e1d
avshukan <avshukan@gmail.com>
2022-01-14 15:30:05 +1100

    step3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const getPath = (filename) => path.resolve(__dirname, filename);
+
+const readFile = (filename) => fs.readFileSync(getPath(filename), 'utf-8');
+
+// Само чтение файлов нужно выполнять либо внутри тестов,
+// либо внутри хуков, например `beforeAll` или `beforeEach`.
+// Не стоит этого делать на уровне модуля, вне функций.
+// const html = await readFile('withLinks.html');
+// const json = await readFile('somethingElse.json');
 
 const program = new Command();
 
@@ -11,8 +28,15 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
-    console.log(`${program.opts().format}: ${diff}`);
+    try {
+      const data1 = readFile(filepath1);
+      const data2 = readFile(filepath2);
+      const diff = genDiff(data1, data2);
+      console.log(diff);
+    } catch (e) {
+      console.error('something was wrong...');
+      console.log(e);
+    }
   });
 
 program.parse();
