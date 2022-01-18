942560d22a2575eca51509846fc06303292f38e0
Alexander Shukan <avshukan@gmail.com>
2022-01-18 17:52:23 +1100

    start tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,9 @@
 import genDiff from '../index.js';
+import file1 from '../__fixtures__/file1.json';
+import file2 from '../__fixtures__/file2.json';
 
 test('reverse', () => {
+  const diff12 = genDiff(file1, file2);
   expect(1).toEqual(1);
+  console.log(diff12);
 });
