d108ec2975e08505c19a8ab566e98f4583568192
Alexander Shukan <avshukan@gmail.com>
2022-01-19 14:12:31 +1100

    finisth tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,34 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
 import genDiff from '../index.js';
-import file1 from '../__fixtures__/file1.json';
-import file2 from '../__fixtures__/file2.json';
 
-test('reverse', () => {
-  const diff12 = genDiff(file1, file2);
-  expect(1).toEqual(1);
-  console.log(diff12);
-});
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const getFixturesPath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+
+const readFile = (filename) => fs.readFileSync(getFixturesPath(filename), 'utf-8');
+
+test.each([
+  ['file0.json', 'file0.json', 'diff00.txt'],
+  ['file0.json', 'file1.json', 'diff01.txt'],
+  ['file0.json', 'file2.json', 'diff02.txt'],
+  ['file1.json', 'file0.json', 'diff10.txt'],
+  ['file1.json', 'file1.json', 'diff11.txt'],
+  ['file1.json', 'file2.json', 'diff12.txt'],
+  ['file2.json', 'file0.json', 'diff20.txt'],
+  ['file2.json', 'file1.json', 'diff21.txt'],
+  ['file2.json', 'file2.json', 'diff22.txt'],
+])(
+  'apply genDiff with %s & %s and expected %s',
+  (filenameBefore, filenameAfter, filenameExpected) => {
+    const fileBefore = readFile(filenameBefore);
+    const fileAfter = readFile(filenameAfter);
+    const expectedResult = readFile(filenameExpected);
+    const realResult = genDiff(fileBefore, fileAfter);
+    expect(realResult).toEqual(expectedResult);
+  },
+);
--- src/genDiff.js
+++ src/genDiff.js
@@ -4,7 +4,6 @@ export default (data1, data2) => {
   const json1 = JSON.parse(data1);
   const json2 = JSON.parse(data2);
   const keys = _.union(_.keys(json1), _.keys(json2)).sort();
-  console.log('keys', keys);
   const diff = keys.map((key) => {
     if (!_.has(json1, key)) { return [`+ ${key}: ${json2[key]}`]; }
     if (!_.has(json2, key)) { return [`- ${key}: ${json1[key]}`]; }
