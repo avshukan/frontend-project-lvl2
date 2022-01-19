b268bd961f8dfa8711891d8aecb1d04c12cb3c60
Alexander Shukan <avshukan@gmail.com>
2022-01-19 16:11:45 +1100

    try yaml

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import genDiff from './src/genDiff.js';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
+
+(() => {
+    const file = fs.readFileSync(path.join( '__fixtures__', 'file1.yaml'));
+    const doc = yaml.load(file);
+    console.log('doc', doc);
+})();
+
 
 export default genDiff;
