d02536c5520c2cd7344d55d7901f7a6677f4486f
avshukan <avshukan@gmail.com>
2022-02-13 23:13:54 +1100

    add new tests

---
+++ __tests__/new.test.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { test, expect, describe } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturesPath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+
+const readFile = (filename) => fs.readFileSync(getFixturesPath(filename), 'utf-8');
+
+const handler = (filenameBefore, filenameAfter) => {
+  const path1 = getFixturesPath(filenameBefore);
+  const path2 = getFixturesPath(filenameAfter);
+  const realStylish = genDiff(path1, path2, 'stylish');
+  const expectedStylish = readFile('expected.stylish');
+  expect(realStylish).toEqual(expectedStylish);
+  const realPlain = genDiff(path1, path2, 'plain');
+  const expectedPlain = readFile('expected.plain');
+  expect(realPlain).toEqual(expectedPlain);
+  const realJson = genDiff(path1, path2, 'json');
+  const expectedJson = readFile('expected.json');
+  expect(JSON.parse(realJson)).toMatchObject(JSON.parse(expectedJson));
+};
+
+describe('all tests', () => {
+  test.each([
+    ['_file1.json', '_file2.json'],
+    // ['_file1.json', '_file2.yml'],
+    // ['_file1.yml', '_file2.json'],
+    // ['_file1.yml', '_file2.yml'],
+  ])('apply genDiff with %s & %s', handler);
+});
