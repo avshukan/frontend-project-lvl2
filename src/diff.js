d6ade1fd4e06aaf933481739ae01ebd946c89171
avshukan <avshukan@gmail.com>
2022-02-13 22:03:11 +1100

    add makeDiffNode

--- src/diff.js
+++ src/diff.js
@@ -8,42 +8,24 @@ const diffStates = {
   COMPLEX: 'COMPLEX',
 };
 
+const makeDiffNode = (name, state, value) => ({ name, state, value });
+
 const makeDiff = (obj1, obj2) => {
   const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
   const result = keys.map((name) => {
     if (!_.has(obj1, name)) {
-      return {
-        name,
-        state: diffStates.ADDED,
-        value: _.cloneDeep(obj2[name]),
-      };
+      return makeDiffNode(name, diffStates.ADDED, _.cloneDeep(obj2[name]));
     }
     if (!_.has(obj2, name)) {
-      return {
-        name,
-        state: diffStates.REMOVED,
-        value: _.cloneDeep(obj1[name]),
-      };
+      return makeDiffNode(name, diffStates.REMOVED, _.cloneDeep(obj1[name]));
     }
     if (_.isPlainObject(obj1[name]) && _.isPlainObject(obj2[name])) {
-      return {
-        name,
-        state: diffStates.COMPLEX,
-        value: makeDiff(obj1[name], obj2[name]),
-      };
+      return makeDiffNode(name, diffStates.COMPLEX, makeDiff(obj1[name], obj2[name]));
     }
     if (obj1[name] === obj2[name]) {
-      return {
-        name,
-        state: diffStates.UNCHANGED,
-        value: obj1[name],
-      };
+      return makeDiffNode(name, diffStates.UNCHANGED, obj1[name]);
     }
-    return {
-      name,
-      state: diffStates.CHANGED,
-      value: [obj1[name], obj2[name]],
-    };
+    return makeDiffNode(name, diffStates.CHANGED, [obj1[name], obj2[name]]);
   });
   return result;
 };
