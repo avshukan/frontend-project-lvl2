dc476d2b661ef89041395e3a3b0d47cd351f78d1
avshukan <avshukan@gmail.com>
2022-02-21 23:39:14 +1100

    getFormatter

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import getParsedData from './parsers.js';
 import makeDiff from './diff.js';
-import formatters from './formatters/index.js';
+import getFormatter from './formatters/index.js';
 
 const getPath = (fileName) => path.resolve(process.cwd(), fileName);
 
@@ -15,7 +15,7 @@ const genDiff = (filepath1, filepath2, formatName) => {
   const path2 = getPath(filepath2);
   const obj1 = getParsedData(getData(path1), getFormat(path1));
   const obj2 = getParsedData(getData(path2), getFormat(path2));
-  const formatter = formatters(formatName);
+  const formatter = getFormatter(formatName);
   const diff = makeDiff(obj1, obj2);
   return formatter(diff);
 };
